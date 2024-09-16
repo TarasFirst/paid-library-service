317c794a81b8ca9875b0daaaf9828b57538bd657
TarasFirst <firsttaras@mail.com>
2024-09-16 09:40:21 +0200

    Create Book list and detail serializers
    - boks/serializers.py.

---
+++ books/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from books.models import Book
+
+
+class BookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "cover", "inventory", "daily_fee"]
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id", "title", "author", "cover", "inventory", "daily_fee"]
