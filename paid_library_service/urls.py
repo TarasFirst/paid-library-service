0be7b4b6c5c23d531a7abeb9a86b2314f0e0ca54
TarasFirst <firsttaras@mail.com>
2024-09-17 16:13:42 +0200

    add path api/users

--- paid_library_service/urls.py
+++ paid_library_service/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls")),
+    path("api/", include("books.urls", namespace="books")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
