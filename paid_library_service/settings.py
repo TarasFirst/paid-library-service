d0b8f73132eb6ad437c42f04ab598edc07f56c2e
TarasFirst <firsttaras@mail.com>
2024-09-23 13:07:15 +0200

    In SIMPLE_JWT change AUTH_HEADER_NAME to AUTHORIZE

--- paid_library_service/settings.py
+++ paid_library_service/settings.py
@@ -144,4 +144,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
+    "AUTH_HEADER_NAME": "AUTHORIZE",
 }
