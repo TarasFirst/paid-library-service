86490fd2b334dd3aeb3f4d6dcc0f87a5f4a18e3f
TarasFirst <firsttaras@mail.com>
2024-09-23 13:44:29 +0200

    Create borrowing viewset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    permission_classes = (IsAuthenticated,)
