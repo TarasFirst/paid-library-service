ef8cdfc4d008b09d4a9703c6bc0e7f5627f5a6d4
TarasFirst <firsttaras@mail.com>
2024-09-25 10:53:29 +0200

    Add a perform_create method to BorrowingViewSet
    - set the `user` field to the currently authenticated user;
    - handle model validation errors and convert them into DRF validation errors.

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,12 +1,42 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError as DRFValidationError
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing book borrowings.
+
+    - This ViewSet allows creating, updating, deleting, and retrieving borrowings.
+    - The `user` field is automatically assigned to the currently authenticated user when creating a borrowing.
+
+    Methods:
+    - `perform_create(self, serializer)`: Saves a new borrowing and associates it with the user.
+      If validation errors occur, they are converted into Django REST Framework (DRF) validation errors.
+
+    """
     queryset = Borrowing.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = BorrowingSerializer
 
+    def perform_create(self, serializer):
+        """
+        Creates a new borrowing entry and associates it with the current user.
+
+        - Sets the `user` field to the currently authenticated user (`self.request.user`).
+        - Handles model validation errors and converts them into DRF validation errors.
+
+        Args:
+            serializer: The serializer instance containing the data for the new borrowing.
+
+        Raises:
+            ValidationError: If validation errors occur, they are raised as DRF ValidationError.
+        """
+        try:
+            serializer.save(user=self.request.user)
+        except DjangoValidationError as e:
+            raise DRFValidationError(detail=e.message_dict)
