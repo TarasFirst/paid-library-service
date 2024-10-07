6141718942c4f2f64756fae4092ee9062f90b552
TarasFirst <firsttaras@mail.com>
2024-10-07 10:54:38 +0200

    Use TextChoices and inner class in defining choices in Book model

--- books/models.py
+++ books/models.py
@@ -2,16 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
-    HARD = "HARD"
-    SOFT = "SOFT"
-    COVER_CHOICES = [
-        (HARD, "Hard Cover"),
-        (SOFT, "Soft Cover"),
-    ]
+    class CoverType(models.TextChoices):
+        HARD = "HARD COVER", "HARD"
+        SOFT = "SOFT COVER", "SOFT"
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=10, choices=COVER_CHOICES)
+    cover = models.CharField(max_length=10, choices=CoverType.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
